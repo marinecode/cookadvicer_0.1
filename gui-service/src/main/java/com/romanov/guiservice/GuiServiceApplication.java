c3cc18818e8af4d9672074423d52837b8092236e
marinecode <artempomanov@gmail.com>
2019-09-02 18:10:43 +0300

    Изменил структуру интерфейса
    Объединил два модуля (ui-front and ui-back) в один (gui-service). Лишнее удалил

---
+++ gui-service/src/main/java/com/romanov/guiservice/GuiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.romanov.guiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuiServiceApplication.class, args);
+    }
+
+}
