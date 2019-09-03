fb076d11758b5087098516ab3eb79e84873fc8e5
marinecode <artempomanov@gmail.com>
2019-09-03 17:47:10 +0300

    Добавил элементы навигации

--- gui-service/src/app/app-routing.module.ts
+++ gui-service/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {RegistrationComponent} from "./registration/registration.component";
+import {MyRecipesComponent} from "./my-recipes/my-recipes.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'login/registration', component: RegistrationComponent},
+  {path: 'recipes', component: MyRecipesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- gui-service/src/app/app.module.ts
+++ gui-service/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import {MatButtonModule, MatButtonToggleModule, MatCardModule, MatToolbarModule} from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import {RouterModule} from "@angular/router";
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { MyRecipesComponent } from './my-recipes/my-recipes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonToggleModule,
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistrationComponent,
+    MyRecipesComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
