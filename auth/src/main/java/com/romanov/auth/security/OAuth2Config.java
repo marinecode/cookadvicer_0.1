2e9c56e7c035c8b2eb9e5c162484d2172cd85841
marinecode <artempomanov@gmail.com>
2019-09-17 16:44:06 +0300

    Попытка №1 не работает получение токена у сервиса аутонтификации

--- auth/src/main/java/com/romanov/auth/security/OAuth2Config.java
+++ auth/src/main/java/com/romanov/auth/security/OAuth2Config.java
@@ -1,7 +1,9 @@
 package com.romanov.auth.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -16,6 +18,7 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
+    @Qualifier("userDetailsServiceBean")
     private UserDetailsService userDetailsService;
 
 
