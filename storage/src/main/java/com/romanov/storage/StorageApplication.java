6227dfc08a48ed2c2d0091ff95be0b690a22500b
marinecode <artempomanov@gmail.com>
2019-09-18 13:09:43 +0300

    Revert "Попытка №1 не работает получение токена у сервиса аутонтификации"
    This reverts commit 2e9c56e7c035c8b2eb9e5c162484d2172cd85841.

--- storage/src/main/java/com/romanov/storage/StorageApplication.java
+++ storage/src/main/java/com/romanov/storage/StorageApplication.java
@@ -3,13 +3,9 @@ package com.romanov.storage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EntityScan( value = {"recipes", "history"})
 @SpringBootApplication
-@EnableResourceServer
-@EnableOAuth2Sso
 public class StorageApplication {
 
     public static void main(String[] args) {
