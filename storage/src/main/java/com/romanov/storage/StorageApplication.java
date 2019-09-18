793f9e3ac7354f08653e8929e0d35be951e197e9
marinecode <artempomanov@gmail.com>
2019-09-18 13:17:06 +0300

    РесурсСервер и AuthServer общаются и все работает

--- storage/src/main/java/com/romanov/storage/StorageApplication.java
+++ storage/src/main/java/com/romanov/storage/StorageApplication.java
@@ -3,9 +3,11 @@ package com.romanov.storage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EntityScan( value = {"recipes", "history"})
 @SpringBootApplication
+@EnableResourceServer
 public class StorageApplication {
 
     public static void main(String[] args) {
