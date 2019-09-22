0d49c443b14197d2d795aba23a66022a8ef09421
marinecode <artempomanov@gmail.com>
2019-09-22 13:52:53 +0300

    FindAllByCreator

--- storage/src/main/java/com/romanov/storage/repos/TypeRepo.java
+++ storage/src/main/java/com/romanov/storage/repos/TypeRepo.java
@@ -3,6 +3,9 @@ package com.romanov.storage.repos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import recipes.Type;
 
+import java.util.List;
+
 public interface TypeRepo extends JpaRepository<Type, String> {
 
+    List<Type> findAllByCreator(String creator );
 }
