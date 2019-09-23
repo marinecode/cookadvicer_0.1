ca6e51b36bd93211864dfc3c91fe727756a59870
marinecode <artempomanov@gmail.com>
2019-09-23 17:03:52 +0300

    Postmen init

---
+++ postman/src/main/java/com/romanov/postman/PostmanApplication.java
@@ -0,0 +1,30 @@
+package com.romanov.postman;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PostmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostmanApplication.class, args);
+    }
+
+    @Autowired
+    EmailServiceImpl emailService;
+
+    @Bean
+    CommandLineRunner runner(){
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                System.out.println("Сейчас пошлю письмо");
+                emailService.sendSimpleMessage( "artempomanov@gmail.com", "Привет из Java", "Весенний привет!");
+                System.out.println("Послал, проверяй");
+            }
+        };
+    }
+}
