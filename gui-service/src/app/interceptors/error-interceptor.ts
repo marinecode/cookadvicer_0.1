70bc29a8cfaadb8761fb1984272a7a7a07a64c08
marinecode <artempomanov@gmail.com>
2019-09-26 11:39:52 +0300

    Если токен протух, то редирект на логин

--- gui-service/src/app/interceptors/error-interceptor.ts
+++ gui-service/src/app/interceptors/error-interceptor.ts
@@ -2,14 +2,21 @@ import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/com
 import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
 import {tap} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
+
+  constructor( private router: Router){}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log( req );
     return next.handle( req ).pipe(
       tap( (event: HttpEvent<any>) => {console.log('CUSTOM INTERCEPTOR'); console.log( event )}
-      , (event: HttpEvent<any>) => {console.log('CUSTOM INTERCEPTOR ERROR'); console.log( event )})
+      , (event: any) => {
+        console.log('CUSTOM INTERCEPTOR ERROR');
+        console.log( event );
+        if( event.status === 401 ) this.router.navigate(["/login"]).then();
+      })
     );
   }
 }
